ce6e317e8f8794733ddf47422bc5ebe97b0721db
YGonzalezSanchez <102389695+AlejandraG28@users.noreply.github.com>
2025-08-23 21:04:58 -0500

    Add files via upload

---
+++ PruebaAplicacion.java
@@ -0,0 +1,13 @@
+package com.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Lugar de ejecucion del codigo
+@SpringBootApplication
+public class PruebaAplicacion {
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaAplicacion.class, args);
+    }
+}
+
